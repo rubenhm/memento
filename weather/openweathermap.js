ca7f13870d11898f28681b70cad2b49cfad548b7
Ruben Hernandez <ruben@rubenhm.org>
2017-09-08 11:40:06 -0400

    Deletes data reference in json result.

--- weather/openweathermap.js
+++ weather/openweathermap.js
@@ -25,29 +25,29 @@ Openweather.prototype.search = function(coord) {
   var resultJson = http().get(baseurl + coord + "&appid=" + this.apiKey );
   var res = JSON.parse(resultJson.body);
   var result = {};
-  if (res.data.id !== undefined)
-      result['id'] = res.data.id;
-  if (res.data.name !== undefined)
-      result['location'] = res.data["name"];
-  if (res.data.weather !== undefined) {
-      result['conditions'] = res.data.weather["main"];
-      result['detail'] = res.data.weather["description"];
-      result['icon'] = "http://openweathermap.org/img/w/" + res.data.weather["icon"] + ".png";
+  if (res.id !== undefined)
+      result['id'] = res["id"];
+  if (res.name !== undefined)
+      result['location'] = res["name"];
+  if (res.weather !== undefined) {
+      result['conditions'] = res.weather["main"];
+      result['detail'] = res.weather["description"];
+      result['icon'] = "http://openweathermap.org/img/w/" + res.weather["icon"] + ".png";
   };
-  if (res.data.main !== undefined) {
-      result['temperature'] = res.data.main["temp"];
-      result['humidity'] = res.data.main["humidity"];
-      result['pressure'] = res.data.main["pressure"];
+  if (res.main !== undefined) {
+      result['temperature'] = res.main["temp"];
+      result['humidity'] = res.main["humidity"];
+      result['pressure'] = res.main["pressure"];
   };
-  if (res.data.wind !== undefined)
-      result['windspeed'] = res.data.wind["speed"];
-  if (res.data.clouds !== undefined)
-      result['clouds'] = res.data.clouds["all"];
-  if (res.data.rain !== undefined)
-      result['rain'] = res.data.rain["3h"];
-  if (res.data.sys !== undefined) {
-      result['sunrise'] = res.data.sys['sunrise'];
-      result['sunset'] = res.data.sys['sunset'];
+  if (res.wind !== undefined)
+      result['windspeed'] = res.wind["speed"];
+  if (res.clouds !== undefined)
+      result['clouds'] = res.clouds["all"];
+  if (res.rain !== undefined)
+      result['rain'] = res.rain["3h"];
+  if (res.sys !== undefined) {
+      result['sunrise'] = res.sys['sunrise'];
+      result['sunset'] = res.sys['sunset'];
   };
   return result;
 }
